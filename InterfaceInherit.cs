d38f5e76d4c3322ee1f71b6baf0b00351a39b691
mate1016 <43951208+mate1016@users.noreply.github.com>
2019-02-21 09:32:47 +0100

    Add files via upload
    Interface Inheritance

---
+++ InterfaceInherit.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+ 
+public interface A
+{
+void mymethod1();
+} 
+public interface B : A
+{ 
+    void mymethod2();
+}
+ 
+class Mateusz : B
+{ 
+    public void mymethod1()
+    {
+        Console.WriteLine("Implement method 1");
+        Console.ReadKey();
+    }
+    public void mymethod2()
+    {
+        Console.WriteLine("Implement method 3");
+        Console.ReadKey();
+    }
+} 
+class Mate
+{ 
+    static void Main(String[] args)
+    {
+ 
+        Mateusz obj = new Mateusz();
+ 
+        obj.mymethod1();
+        obj.mymethod2();
+    }
+}
